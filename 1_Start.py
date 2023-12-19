cb4133faed76a8f970328beec657d2c5cf90a737
Raka1020 <154009411+Raka1020@users.noreply.github.com>
2023-12-19 20:24:44 +0700

    Update 1_Start.py

--- 1_Start.py
+++ 1_Start.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Home",page_icon="⭐")
+st.set_page_config(page_title="Katrol")
 st.title("PERHITUNGAN HUKUM NEWTON II PADA KATROL")
 
 st.markdown("""
